91adec1abcdc8f8c27d6f9d10deb427f74bb7cbe
Joe Steinkamp <jsteinka@gmail.com>
2026-04-26 19:24:24 -0500

    add xcode project

---
+++ SketchToWeb/SketchToWebApp.swift
@@ -0,0 +1,17 @@
+//
+//  SketchToWebApp.swift
+//  SketchToWeb
+//
+//  Created by Joe Steinkamp on 4/26/26.
+//
+
+import SwiftUI
+
+@main
+struct SketchToWebApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SketchToWebTests/SketchToWebTests.swift
@@ -0,0 +1,17 @@
+//
+//  SketchToWebTests.swift
+//  SketchToWebTests
+//
+//  Created by Joe Steinkamp on 4/26/26.
+//
+
+import Testing
+@testable import SketchToWeb
+
+struct SketchToWebTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
